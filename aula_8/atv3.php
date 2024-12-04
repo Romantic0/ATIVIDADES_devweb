d1d67503c166d0625f825f03e723abbfef22fad4
Guilherme Afonso Casa <guylhermeeafonso@gmail.com>
2024-12-03 22:13:04 -0300

    pronto

--- aula_8/atv3.php
+++ aula_8/atv3.php
@@ -1,13 +1,13 @@
 <?php
-// Declaração e atribuição inicial
+
 $SALARIO1 = 1000;
 $SALARIO2 = 2000;
 
-// Atribuir o valor de SALARIO1 para SALARIO2 e incrementar
+
 $SALARIO2 = $SALARIO1;
 $SALARIO2++;
 
-// Adicionar 10% de aumento para SALARIO1
+
 $SALARIO1 += $SALARIO1 * 0.1;
 
 // Comparação
